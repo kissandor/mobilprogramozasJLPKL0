960d6de36fb0c952cb0178696c3377155f6cce2a
Sandor Kis <kisandor@hotmail.com>
2022-12-03 20:47:54 +0000

    info fragment created

---
+++ app/src/main/java/com/example/mobilprogramozasjlpkl0/info.java
@@ -0,0 +1,29 @@
+package com.example.mobilprogramozasjlpkl0;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class info extends Fragment {
+
+    public info() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+        return view;
+    }
+}
