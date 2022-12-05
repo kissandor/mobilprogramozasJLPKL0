8e8250b090648a8f9cc67cac55ac191b2829d590
Sandor Kis <kisandor@hotmail.com>
2022-12-05 19:56:47 +0000

    send email implemented

--- app/src/main/java/com/example/mobilprogramozasjlpkl0/info.java
+++ app/src/main/java/com/example/mobilprogramozasjlpkl0/info.java
@@ -1,5 +1,7 @@
 package com.example.mobilprogramozasjlpkl0;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,10 +10,15 @@ import androidx.fragment.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.resources.TextAppearance;
 
 
 public class info extends Fragment {
+
+    TextView email;
 
     public info() {
         // Required empty public constructor
@@ -26,6 +33,18 @@ public class info extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_info, container, false);
+
+        email = view.findViewById(R.id.textView_email);
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:" + email.getText().toString()));
+                //intent.putExtra(Intent.EXTRA_EMAIL, email.getText());
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                getContext().startActivity(intent);
+            }
+        });
 
         view.setOnClickListener(new View.OnClickListener() {
             @Override
